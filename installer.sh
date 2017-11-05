849befb36791f2f90071c0cabf37b27f9f86b866
Txema León <soy@txemaleon.com>
2017-11-06 00:43:02 +0100

    Install installs dotfiles and Sublime Config

--- installer.sh
+++ installer.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-
 sudo -v
 
+# Install HomeBrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew bundle
+
+# Install dotfiles, Terminal config, and Sublime Text Configs
+mkdir -p ~/.config/
+git clone git@github.com:txemaleon/dotfiles.git ~/.config/dotfiles
+mkdir -p ~/Library/Application Support/Sublime Text 3/Packages/User/
+git clone git@github.com:txemaleon/Sublime-Text.git ~/Library/Application Support/Sublime Text 3/Packages/User/
+ln -s ~/Library/Application Support/Sublime Text 3/Packages/User/ ~/.config/sublime
+
+# Install node tools
 npm i -g expo fixpack mversion prettier yarn
