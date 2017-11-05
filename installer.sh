0f7e1ed9c2209d5cd178fd2363bc4083c9f61985
Txema León <soy@txemaleon.com>
2017-11-06 00:59:25 +0100

    Add autolink from dotfiles

--- installer.sh
+++ installer.sh
@@ -8,9 +8,13 @@ brew bundle
 # Install dotfiles, Terminal config, and Sublime Text Configs
 mkdir -p ~/.config/
 git clone git@github.com:txemaleon/dotfiles.git ~/.config/dotfiles
+for f in bash_profile fixpackrc gitconfig gitignore inputrc npmrc vimrc yarnrc slate ; do ln -s ~/.config/dotfiles/$f ~/.$f; done
+
+# Sublime Text Config
 mkdir -p ~/Library/Application Support/Sublime Text 3/Packages/User/
 git clone git@github.com:txemaleon/Sublime-Text.git ~/Library/Application Support/Sublime Text 3/Packages/User/
 ln -s ~/Library/Application Support/Sublime Text 3/Packages/User/ ~/.config/sublime
+
 
 # Install node tools
 npm i -g expo fixpack mversion prettier yarn
