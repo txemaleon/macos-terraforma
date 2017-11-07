7c0f99b267b0431ad4f20fc8f20a104c5bfd669c
Txema León <soy@txemaleon.com>
2017-11-07 11:07:08 +0100

    Update installer to configure macos

--- installer.sh
+++ installer.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# Ask for the administrator password upfront
 sudo -v
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Configure macos
+. ./macos.sh
 
 # Install HomeBrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -11,10 +18,9 @@ git clone git@github.com:txemaleon/dotfiles.git ~/.config/dotfiles
 for f in bash_profile fixpackrc gitconfig gitignore inputrc npmrc vimrc yarnrc slate ; do ln -s ~/.config/dotfiles/$f ~/.$f; done
 
 # Sublime Text Config
-mkdir -p ~/Library/Application Support/Sublime Text 3/Packages/User/
-git clone git@github.com:txemaleon/Sublime-Text.git ~/Library/Application Support/Sublime Text 3/Packages/User/
-ln -s ~/Library/Application Support/Sublime Text 3/Packages/User/ ~/.config/sublime
-
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+git clone git@github.com:txemaleon/Sublime-Text.git ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+ln -s ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/ ~/.config/sublime
 
 # Install node tools
 npm i -g expo fixpack mversion prettier yarn
