47eb24b8116b305f276ef387d3efc62731712223
Txema León <soy@txemaleon.com>
2017-12-25 16:38:47 +0100

    Add Tmux Conf

--- installer.sh
+++ installer.sh
@@ -15,9 +15,10 @@ brew bundle
 # Install dotfiles, Terminal config, and Sublime Text Configs
 mkdir -p ~/.config/
 git clone git@github.com:txemaleon/dotfiles.git ~/.config/dotfiles
-for f in bash_profile fixpackrc gitconfig gitignore inputrc npmrc vimrc yarnrc slate ; do ln -s ~/.config/dotfiles/$f ~/.$f; done
+for f in bash_profile fixpackrc gitconfig inputrc npmrc slate tmux.conf vimrc yarnrc; do ln -s ~/.config/dotfiles/$f ~/.$f; done
 
 # Vim Config
+mkdir -p ~/.vim/bundle/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
